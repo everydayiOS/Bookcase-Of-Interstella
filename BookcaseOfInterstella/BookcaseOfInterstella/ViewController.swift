16824d79bf3bda1bc9707b4724ea2c1597162f19
dev-Lena <keunnalee@gmail.com>
2020-09-17 13:47:56 +0900

    Feat: Xcode 프로젝트 추가
    - Use Core Data
    
    #1

---
+++ BookcaseOfInterstella/BookcaseOfInterstella/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookcaseOfInterstella
+//
+//  Created by Keunna Lee on 2020/09/17.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
