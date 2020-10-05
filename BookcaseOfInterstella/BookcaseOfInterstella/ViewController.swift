b39c7d7852a7cb37bc42e97d46f93f997aa01ad9
wonhee <wonhee010@gmail.com>
2020-10-06 01:08:39 +0900

    feat: 임시 object 추가

--- BookcaseOfInterstella/BookcaseOfInterstella/ViewController.swift
+++ BookcaseOfInterstella/BookcaseOfInterstella/ViewController.swift
@@ -15,4 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // 글쓰기
+    @IBAction func writeButtonDidTap(_ sender: Any) {
+        
+    }
+    
+    // 즐겨찾기 편지 목록
+    @IBAction func bookmarkButtonDidTap(_ sender: Any) {
+        
+    }
+    
+    // 임시보관 편지 목록
+    @IBAction func temporaryButtonDidTap(_ sender: Any) {
+        
+    }
+    
+    // 편지 도착 화면
+    @IBAction func arrivalButtonDidTap(_ sender: Any) {
+        
+    }
+    
+    //설정
+    @IBAction func settingButtonDidTap(_ sender: Any) {
+        
+    }
 }
